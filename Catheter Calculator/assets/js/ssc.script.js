9cea7bd3c251db7c9d8f545f814768ac82aabfe9
jsandos <jasonsandos@gmail.com>
2019-02-14 15:58:11 -0700

    Add files via upload

---
+++ Catheter Calculator/assets/js/ssc.script.js
@@ -0,0 +1,171 @@
+/*!calc*/var eecm1=new Array();function calc(data){var c1E23=data['XLEW_1_23_5'];var c1E3=data['XLEW_1_3_5'];var tmp1=(((c1E3)*(7)));var sumcnt2_sum=0+tmp1;var sumcnt2_cnt=1;var c1B6=(sumgeneral(0,sumcnt2_sum,sumcnt2_cnt,eecm1));var tmp5=(((c1E3)*(14)));var sumcnt6_sum=0+tmp5;var sumcnt6_cnt=1;var c1B8=(sumgeneral(0,sumcnt6_sum,sumcnt6_cnt,eecm1));var tmp9=(((c1E3)*(30)));var sumcnt10_sum=0+tmp9;var sumcnt10_cnt=1;var c1B10=(sumgeneral(0,sumcnt10_sum,sumcnt10_cnt,eecm1));var tmp13=(((c1E3)*(92)));var sumcnt14_sum=0+tmp13;var sumcnt14_cnt=1;var c1B12=(sumgeneral(0,sumcnt14_sum,sumcnt14_cnt,eecm1));var tmp17=(((c1E3)*(182)));var sumcnt18_sum=0+tmp17;var sumcnt18_cnt=1;var c1B14=(sumgeneral(0,sumcnt18_sum,sumcnt18_cnt,eecm1));var tmp21=(((c1E3)*(365)));var sumcnt22_sum=0+tmp21;var sumcnt22_cnt=1;var c1B16=(sumgeneral(0,sumcnt22_sum,sumcnt22_cnt,eecm1));var tmp25=(((c1E23)*(100)));var sumcnt26_sum=0+tmp25;var sumcnt26_cnt=1;var c1B26=(sumgeneral(0,sumcnt26_sum,sumcnt26_cnt,eecm1));var tmp29=(((c1E23)*(200)));var sumcnt30_sum=0+tmp29;var sumcnt30_cnt=1;var c1B28=(sumgeneral(0,sumcnt30_sum,sumcnt30_cnt,eecm1));var tmp33=(((c1E23)*(300)));var sumcnt34_sum=0+tmp33;var sumcnt34_cnt=1;var c1B30=(sumgeneral(0,sumcnt34_sum,sumcnt34_cnt,eecm1));var tmp37=(((c1E23)*(400)));var sumcnt38_sum=0+tmp37;var sumcnt38_cnt=1;var c1B32=(sumgeneral(0,sumcnt38_sum,sumcnt38_cnt,eecm1));var tmp41=(((c1E23)*(500)));var sumcnt42_sum=0+tmp41;var sumcnt42_cnt=1;var c1B34=(sumgeneral(0,sumcnt42_sum,sumcnt42_cnt,eecm1));
+ var tmp45=(((c1E23)*(600)));var sumcnt46_sum=0+tmp45;var sumcnt46_cnt=1;var c1B36=(sumgeneral(0,sumcnt46_sum,sumcnt46_cnt,eecm1));var tmp49=(((c1E23)*(700)));var sumcnt50_sum=0+tmp49;var sumcnt50_cnt=1;var c1B38=(sumgeneral(0,sumcnt50_sum,sumcnt50_cnt,eecm1));var tmp53=(((c1E23)*(800)));var sumcnt54_sum=0+tmp53;var sumcnt54_cnt=1;var c1B40=(sumgeneral(0,sumcnt54_sum,sumcnt54_cnt,eecm1));var tmp57=(((c1E23)*(900)));var sumcnt58_sum=0+tmp57;var sumcnt58_cnt=1;var c1B42=(sumgeneral(0,sumcnt58_sum,sumcnt58_cnt,eecm1));var tmp61=(((c1E23)*(1000)));var sumcnt62_sum=0+tmp61;var sumcnt62_cnt=1;var c1B44=(sumgeneral(0,sumcnt62_sum,sumcnt62_cnt,eecm1));var tmp3=(((c1B6)/(100)));var sumcnt4_sum=0+tmp3;var sumcnt4_cnt=1;var c1D6=(sumgeneral(0,sumcnt4_sum,sumcnt4_cnt,eecm1));var tmp7=(((c1B8)/(100)));var sumcnt8_sum=0+tmp7;var sumcnt8_cnt=1;var c1D8=(sumgeneral(0,sumcnt8_sum,sumcnt8_cnt,eecm1));var tmp11=(((c1B10)/(100)));var sumcnt12_sum=0+tmp11;var sumcnt12_cnt=1;var c1D10=(sumgeneral(0,sumcnt12_sum,sumcnt12_cnt,eecm1));var tmp15=(((c1B12)/(100)));var sumcnt16_sum=0+tmp15;var sumcnt16_cnt=1;var c1D12=(sumgeneral(0,sumcnt16_sum,sumcnt16_cnt,eecm1));var tmp19=(((c1B14)/(100)));var sumcnt20_sum=0+tmp19;var sumcnt20_cnt=1;var c1D14=(sumgeneral(0,sumcnt20_sum,sumcnt20_cnt,eecm1));var tmp23=(((c1B16)/(100)));var sumcnt24_sum=0+tmp23;var sumcnt24_cnt=1;var c1D16=(sumgeneral(0,sumcnt24_sum,sumcnt24_cnt,eecm1));
+ var tmp27=(((c1B26)/(144)));var sumcnt28_sum=0+tmp27;var sumcnt28_cnt=1;var c1D26=(sumgeneral(0,sumcnt28_sum,sumcnt28_cnt,eecm1));var tmp31=(((c1B28)/(144)));var sumcnt32_sum=0+tmp31;var sumcnt32_cnt=1;var c1D28=(sumgeneral(0,sumcnt32_sum,sumcnt32_cnt,eecm1));var tmp35=(((c1B30)/(144)));var sumcnt36_sum=0+tmp35;var sumcnt36_cnt=1;var c1D30=(sumgeneral(0,sumcnt36_sum,sumcnt36_cnt,eecm1));var tmp39=(((c1B32)/(144)));var sumcnt40_sum=0+tmp39;var sumcnt40_cnt=1;var c1D32=(sumgeneral(0,sumcnt40_sum,sumcnt40_cnt,eecm1));var tmp43=(((c1B34)/(144)));var sumcnt44_sum=0+tmp43;var sumcnt44_cnt=1;var c1D34=(sumgeneral(0,sumcnt44_sum,sumcnt44_cnt,eecm1));var tmp47=(((c1B36)/(144)));var sumcnt48_sum=0+tmp47;var sumcnt48_cnt=1;var c1D36=(sumgeneral(0,sumcnt48_sum,sumcnt48_cnt,eecm1));var tmp51=(((c1B38)/(144)));var sumcnt52_sum=0+tmp51;var sumcnt52_cnt=1;var c1D38=(sumgeneral(0,sumcnt52_sum,sumcnt52_cnt,eecm1));var tmp55=(((c1B40)/(144)));var sumcnt56_sum=0+tmp55;var sumcnt56_cnt=1;var c1D40=(sumgeneral(0,sumcnt56_sum,sumcnt56_cnt,eecm1));var tmp59=(((c1B42)/(144)));var sumcnt60_sum=0+tmp59;var sumcnt60_cnt=1;var c1D42=(sumgeneral(0,sumcnt60_sum,sumcnt60_cnt,eecm1));var tmp63=(((c1B44)/(144)));var sumcnt64_sum=0+tmp63;var sumcnt64_cnt=1;var c1D44=(sumgeneral(0,sumcnt64_sum,sumcnt64_cnt,eecm1));data['XLEW_1_44_4']=c1D44;data['XLEW_1_44_2']=c1B44;data['XLEW_1_42_4']=c1D42;
+ data['XLEW_1_42_2']=c1B42;data['XLEW_1_40_4']=c1D40;data['XLEW_1_40_2']=c1B40;data['XLEW_1_38_4']=c1D38;data['XLEW_1_38_2']=c1B38;data['XLEW_1_36_4']=c1D36;data['XLEW_1_36_2']=c1B36;data['XLEW_1_34_4']=c1D34;data['XLEW_1_34_2']=c1B34;data['XLEW_1_32_4']=c1D32;data['XLEW_1_32_2']=c1B32;data['XLEW_1_30_4']=c1D30;data['XLEW_1_30_2']=c1B30;data['XLEW_1_28_4']=c1D28;data['XLEW_1_28_2']=c1B28;data['XLEW_1_26_4']=c1D26;data['XLEW_1_26_2']=c1B26;data['XLEW_1_16_4']=c1D16;data['XLEW_1_16_2']=c1B16;data['XLEW_1_14_4']=c1D14;data['XLEW_1_14_2']=c1B14;data['XLEW_1_12_4']=c1D12;data['XLEW_1_12_2']=c1B12;data['XLEW_1_10_4']=c1D10;data['XLEW_1_10_2']=c1B10;data['XLEW_1_8_4']=c1D8;data['XLEW_1_8_2']=c1B8;data['XLEW_1_6_4']=c1D6;data['XLEW_1_6_2']=c1B6;};
+var eeisus=1;var eetrue="TRUE";var eefalse="FALSE";var eedec=".";var eeth=",";var eedecreg=new RegExp("\\.","g");var eethreg=new RegExp(",","g");var eecurrencyreg=new RegExp("[$]","g");var eepercentreg=new RegExp("%","g");
+var co = new Object;
+function recalc_onclick(ctl) {ssc.recalcSource = ctl;
+  if (true) {
+
+
+co['XLEW_1_3_5']=eeparseFloat(document.getElementById('XLEW_1_3_5').value);co['XLEW_1_23_5']=eeparseFloat(document.getElementById('XLEW_1_23_5').value);calc(co);if(ssc !== undefined && ssc.realtimesync !== undefined && ssc.realtimesync.connection !== undefined && ssc.realtimesync.connection.readyState == 1){ssc.realtimesync.coUpdate(co,ctl);}else if(ssc !== undefined && ssc.realtimesync !== undefined && ssc.realtimesync.wsUrl && ssc.realtimesync.IsCloseByError && !ssc.realtimesync.IsErrorMsgDialogShown){if(!alert('Realtime-Sync is Disconnected, This Form will not save in server and will not collaborate with your partner.')){ssc.realtimesync.IsErrorMsgDialogShown = true;}}document.getElementById('XLEW_1_6_2').value=eedisplayFloat(co['XLEW_1_6_2']);document.getElementById('XLEW_1_6_4').value=eedisplayFloat(co['XLEW_1_6_4']);document.getElementById('XLEW_1_8_2').value=eedisplayFloat(co['XLEW_1_8_2']);document.getElementById('XLEW_1_8_4').value=eedisplayFloat(co['XLEW_1_8_4']);document.getElementById('XLEW_1_10_2').value=eedisplayFloat(co['XLEW_1_10_2']);document.getElementById('XLEW_1_10_4').value=eedisplayFloat(co['XLEW_1_10_4']);document.getElementById('XLEW_1_12_2').value=eedisplayFloat(co['XLEW_1_12_2']);document.getElementById('XLEW_1_12_4').value=eedisplayFloat(co['XLEW_1_12_4']);document.getElementById('XLEW_1_14_2').value=eedisplayFloat(co['XLEW_1_14_2']);document.getElementById('XLEW_1_14_4').value=eedisplayFloat(co['XLEW_1_14_4']);document.getElementById('XLEW_1_16_2').value=eedisplayFloat(co['XLEW_1_16_2']);document.getElementById('XLEW_1_16_4').value=eedisplayFloat(co['XLEW_1_16_4']);document.getElementById('XLEW_1_26_2').value=eedisplayFloat(co['XLEW_1_26_2']);document.getElementById('XLEW_1_26_4').value=eedisplayFloatND(co['XLEW_1_26_4'],2);document.getElementById('XLEW_1_28_2').value=eedisplayFloat(co['XLEW_1_28_2']);document.getElementById('XLEW_1_28_4').value=eedisplayFloatND(co['XLEW_1_28_4'],2);document.getElementById('XLEW_1_30_2').value=eedisplayFloat(co['XLEW_1_30_2']);
+document.getElementById('XLEW_1_30_4').value=eedisplayFloatND(co['XLEW_1_30_4'],2);document.getElementById('XLEW_1_32_2').value=eedisplayFloat(co['XLEW_1_32_2']);document.getElementById('XLEW_1_32_4').value=eedisplayFloatND(co['XLEW_1_32_4'],2);document.getElementById('XLEW_1_34_2').value=eedisplayFloat(co['XLEW_1_34_2']);document.getElementById('XLEW_1_34_4').value=eedisplayFloatND(co['XLEW_1_34_4'],2);document.getElementById('XLEW_1_36_2').value=eedisplayFloat(co['XLEW_1_36_2']);document.getElementById('XLEW_1_36_4').value=eedisplayFloatND(co['XLEW_1_36_4'],2);document.getElementById('XLEW_1_38_2').value=eedisplayFloat(co['XLEW_1_38_2']);document.getElementById('XLEW_1_38_4').value=eedisplayFloatND(co['XLEW_1_38_4'],2);document.getElementById('XLEW_1_40_2').value=eedisplayFloat(co['XLEW_1_40_2']);document.getElementById('XLEW_1_40_4').value=eedisplayFloatND(co['XLEW_1_40_4'],2);document.getElementById('XLEW_1_42_2').value=eedisplayFloat(co['XLEW_1_42_2']);document.getElementById('XLEW_1_42_4').value=eedisplayFloatND(co['XLEW_1_42_4'],2);document.getElementById('XLEW_1_44_2').value=eedisplayFloat(co['XLEW_1_44_2']);document.getElementById('XLEW_1_44_4').value=eedisplayFloatND(co['XLEW_1_44_4'],2);
+};};
+function myIsNaN(x){return(isNaN(x)||(typeof x=='number'&&!isFinite(x)));};
+
+function round(n,nd){if(isFinite(n)&&isFinite(nd)){var sign_n=(n<0)?-1:1;var abs_n=Math.abs(n);var factor=Math.pow(10,nd);return sign_n*Math.round(abs_n*factor)/factor;}else{return NaN;}};
+
+function sum2(arr,rt,rb){var sum=0;for(var ii=rt;ii<=rb;ii++){sum+=arr[ii]};return sum};function sum3(arr,rt,ct,rb,cb){var sum=0;for(var ii=rt;ii<=rb;ii++){for(var jj=ct;jj<=cb;jj++){sum+=arr[ii][jj]}};return sum};
+
+function sumgeneral(cnt,vsum,vcnt,x){var sum=vsum;for(var ii=0;ii<x.length;ii++){sum+=sum3(x[ii][0],x[ii][1],x[ii][2],x[ii][3],x[ii][4]);};return sum;};
+
+function eeparseFloat(str){str=String(str).replace(eedecreg,".");var res=parseFloat(str);if(isNaN(res)){return 0;}else{return res;}};
+
+var near0RegExp=new RegExp("[.](.*0000000|.*9999999)");function eedisplayFloat(x){if(myIsNaN(x)){return Number.NaN;}else{var str=String(x);if(near0RegExp.test(str)){x=round(x,8);str=String(x);}return str.replace(/\./g,eedec);}};
+
+function eedisplayFloatND(x,nd){if(myIsNaN(x)){return Number.NaN;}else{var res=round(x,nd);if(nd>0){var str=String(res);if(str.indexOf('e')!=-1)return str;if(str.indexOf('E')!=-1)return str;var parts=str.split('.');if(parts.length<2){var decimals=('00000000000000').substring(0,nd);return(parts[0]).toString()+eedec+decimals;}else{var decimals=((parts[1]).toString()+'00000000000000').substring(0,nd);return(parts[0]).toString()+eedec+decimals;}}else{return res;}}};
+
+function eeisstring(v){switch(typeof v){case "string":return true;case "object":return v.constructor==String;default:return false;}};
+function postcode() {
+};function TriggerOnchange(hiddenId)
+{
+    var e = jQuery.Event('change');
+    $('#'+hiddenId).trigger(e);
+}
+function printClick(){
+           recalc_onclick('');		  
+           $('#printAllTheme').remove();
+           window.print();
+		}
+function LoadFromQueryString() {var queryStrings = decodeURI(document.location.search), _readonly = !1;queryStrings.indexOf("_readonly") > -1 && (_readonly = !0), queryStrings.length > 0 && $.each(queryStrings.substr(1).split("&"), function (c, q) {var aQueryString = q.split("=");if (2 == aQueryString.length) {var key = aQueryString[0].toString(), value = aQueryString[1].toString();ssc.formBinder.bind(key,value);}})}
+function navigate(e){var t=e.keyCode|e.which;if(t!=13&&t!=40&&t!=38)return;var n=$(e.target||e.srcElement);if(n.is("textarea"))return;if(n.is("select")&&(t==38||t==40))return;var r=parseInt(n.attr("data-sheet"),10);var i=parseInt(n.attr("data-row"),10);var s=parseInt(n.attr("data-col"),10);var o=false;var u=$("#sheet-"+r+"");var a='input:not(":hidden,:button,[readonly=readonly],:disabled"),select,a.ui-slider-handle,textarea';var f=i+1;var l=i-1;var c=f;var h=l;while(t==40&&f<=c||t==38&&l>=h){var p=u.find(a).filter("[data-sheet="+r+"][data-row="+(t==38?l:f)+"][data-col="+s+"]");if(p.length>0){p.focus();if(p.is("input:text, textarea"))p.select();o=true;break}else{var d;if(u.data("col"+s)==undefined){d=u.find(a).filter("[data-sheet="+r+"][data-col="+s+"]").map(function(){return parseInt($(this).attr("data-row"),10)}).toArray();u.data("col"+s,d)}else{d=u.data("col"+s)}c=d[d.length-1];h=d[0];var v="indexOf"in Array.prototype?d.indexOf(i):$.inArray(i,d);if(t==40&&i<c){f=d[v+1]}else if(t==38&&i>h){l=d[v-1]}else{break}}}if(!o){var m;if(t==38)m=parseInt(n.attr("tabindex"),10)-1;else m=parseInt(n.attr("tabindex"),10)+1;var g=u.find(a).filter("[data-sheet][data-row][data-col][tabindex="+m+"]");if(g.length>0){if(g.is(":radio"))g=g.filter(":checked");if(g.length>0){g.focus();if(g.is("input:text, textarea"))g.select()}}else{var y=u.find(a).filter("[data-sheet][data-row][data-col][tabindex]:first");y.focus();if(y.is("input:text, textarea"))y.select()}}e.preventDefault?e.preventDefault():e.returnValue=false}
+/* Finds out if the element is hidden by its parent tr */
+                    jQuery.expr[':'].hiddenByParent = function(a) {
+                        /* Skip the hidden form elements */
+                        if(a.id.startsWith('xl_')) return true;
+                        var isHiddenByParent=false;        
+                        var elem = $(a);
+                        if(elem !=undefined)
+                        {
+                            /* if the row is hidden */
+                            isHiddenByParent = elem.closest("tr[class^='_css']").css('display') != 'none';
+                        }
+
+                        if(isHiddenByParent == true)
+                        {
+                            /* if whole tab is hidden */
+                            isHiddenByParent = elem.closest('.tab-pane>div').css('display') != 'none';
+                        }
+                        return isHiddenByParent;                        
+                    };var ssccf1n = function(op,id,css,data1){	
+	                switch(op)
+	                {
+                        						case 'image':                               
+                            $('#' + id).attr('src',$('#'+data1).attr('src'));
+                        break;
+      		
+	                }
+                }; 
+                var ssccf4n = function(op,id,colors,data1,data2,data3,data4){
+                    switch(op)
+                    {
+                           
+                    }
+                };
+
+/* Licensed under the SpreadsheetConverter *//*!
+ * ssc.script.js
+ */
+var ssc=ssc||{};ssc.pendingUploads=0;ssc.recalcSource='';ssc.APPNAME='Catheter Calculator';ssc.exMsg='The fields with the red border are required or invalid.' 
+$(window).on('scroll', function() {
+                    var $w = $(window);
+  $('.position-fixed-x').css('left', $w.scrollLeft());
+  $('.position-fixed-y').css('top', $w.scrollTop());
+                });
+ssc.Dialog={};ssc.Dialog.BeforeSubmitDesc='The form is being submitted.';ssc.Dialog.OfflineHeading='Save until online';ssc.Dialog.OfflineDesc='You are currently offline and the submit failed. Do you want to save the submit and send it later when you are online.';
+                ssc.Dialog.OfflineConfirm='Do you want to save?';ssc.Dialog.OfflineSubmitHeading='Offline forms submit confirmation';ssc.Dialog.OfflineSubmitDesc='There are Offline form(s), which are now ready to submit in server.';ssc.Dialog.OfflineSubmitConfirm='Do you want to submit?';
+                ssc.Dialog.FailOfflineHeading='Offline Form submit failed';ssc.Dialog.FailOfflineDesc='Unable to connect to the Internet. Please try submitting the offline forms later in internet connection.';ssc.Dialog.OfflineSubmitWait='It may take sometime to finish all submits depending on the size of offline forms and internet connection.';ssc.Dialog.OfflineSubmitWaitCounter='Left';
+     var focused=false;
+        (function($){
+            var calendar_dateFormats = {};
+            var defaultDateFmt = 'm/d/yyyy';
+            postcode();
+ssc.active_sheet_selector = 'div.active > div.container-fluid.ssc-responsive-sheet > div.row > section';
+
+$('a').on('click',function (e) {	
+				var href = $(this).attr('href');
+				if(href.slice(0,1) == '#'){				
+					e.preventDefault();	
+                    if(href=='#') return;
+					var $target = $(href);
+					if($target.length == 0){
+						alert('Oops! This link is broken so can not navigate.');
+						return;
+					}
+					focused=false;
+					ssc.focusOnElement($target);
+				}
+			});
+
+                ssc.focusOnElement = function(elem){
+                    if(focused) return;
+                    focused=true;
+                    var tab = elem.parents('.tab-pane');
+				    if(!tab.hasClass('active')){
+					    $('.nav-tabs a[href="#' + tab.attr('id') + '"]').tab('show');
+				    }
+
+				    $('html, body').stop().animate(
+				    {
+					    'scrollTop': elem.offset().top - 40			
+				    }, 
+				    900, 
+				    'swing', 
+				    function () {
+					    elem.focus();						
+				    });
+                }
+$("input[name = 'XLEW_1_3_5']").TouchSpin({
+                min:0,max:100,step:1,
+            }); 
+$("input[name = 'XLEW_1_23_5']").TouchSpin({
+                min:0,max:100,step:1,
+            }); 
+
+			$('.btn.btn-sm.button-reset').on('click',function(){resetClick(this);})
+			$('.btn.btn-sm.button-print.one').on('click',function(){printClick(this);})
+
+            var focusCtrl=$('input:not(":hidden,:button,[readonly=readonly],:disabled"),select,a.ui-slider-handle,textarea,a.ui-state-active');
+			                //to prevent popup dialog to appear in page load.
+			                if(focusCtrl!= undefined && focusCtrl.filter(':first').hasClass('form-control')==false){
+				                focusCtrl.filter(':first').focus();
+			                }
+                            //To make readonly elements background transparent
+                            $('textarea[readonly],input[type=text][readonly]').css('background','transparent');
+        })($);
+ssc.checkIfMSIE = function() {
+            var a = window.navigator.userAgent,
+                b = a.indexOf('MSIE '),
+                c = a.indexOf('.NET CLR');
+            return !(-1 === b && -1 === c)
+        };
+
+            function showValidateMessage(message,title){
+                if(toastr){if(title && title.length>0){toastr.error(message,title);}else{toastr.error(message);}}else{alert(message);}
+            }
+function resetClick(){
+    if(ssc !== undefined && ssc.realtimesync !== undefined){
+        if(ssc.realtimesync.connection !== undefined){
+            ssc.realtimesync.connection.close();ssc.realtimesync.connected=false;
+            ssc.realtimesync.ShowDisconnectAlert($('.toolbar-panel-right'));}}
+            document.formc.reset();
+            postcode();
+            resetSlider('XLEW_1_3_5', 0);			
+			resetSlider('XLEW_1_23_5', 0);
+            LoadFromQueryString();
+            recalc_onclick('reset');
+        }
+
+
+function resetSlider(sliderId,val){$('#'+sliderId).TouchSpin('setValue',val);}
+
